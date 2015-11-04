7eef4b8322858f7e39981261d167049524130f4b
Tomas Korcak <korczis@gmail.com>
2015-11-04 15:49:42 -0800

    Implemented Download, Extract & Transform

--- lib/jetel/modules/ip/ip.rb
+++ lib/jetel/modules/ip/ip.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 require 'pmap'
+require 'csv'
 
 require_relative '../../config/config'
 require_relative '../../helpers/helpers'
@@ -36,20 +37,69 @@ module Jetel
         }
       ]
 
-      def download
+      def download(global_options, options, args)
         SOURCES.pmap do |source|
-          target_dir = Helper.target_dir(self, source)
-          downloader.download(source[:url], {:dir => target_dir})
+          download_source(source, global_options.merge(options))
         end
       end
 
-      def extract
+      def extract(global_options, options, args)
+        SOURCES.pmap do |source|
+          downloaded_file = downloaded_file(source, global_options.merge(options))
+          dest_dir = extract_dir(source, global_options.merge(options))
+
+          puts "Extracting #{downloaded_file}"
+
+          FileUtils.mkdir_p(dest_dir)
+          FileUtils.cp(downloaded_file, dest_dir)
+        end
       end
 
-      def transform
+      def transform(global_options, options, args)
+        SOURCES.pmap do |source|
+          opts = global_options.merge(options)
+
+          extracted_file = extracted_file(source, opts)
+          transformed_file = transformed_file(source, opts)
+
+          FileUtils.mkdir_p(transform_dir(source, opts))
+
+          csv_opts = {
+            :col_sep => '|'
+          }
+
+          puts "Transforming #{extracted_file}"
+          CSV.open(extracted_file, 'r', csv_opts) do |csv|
+            headers = %w(
+              registry
+              cc
+              type
+              start
+              value
+              date
+              status
+            )
+            CSV.open(transformed_file, 'w', :write_headers => true, :headers => headers) do |csv_out|
+              csv.each do |row|
+                next if row.length < 7
+                next if row[0] != source[:name]
+                csv_out << row
+              end
+            end
+          end
+        end
       end
 
-      def load
+      def load(global_options, options, args)
+        SOURCES.map do |source|
+          opts = global_options.merge(options)
+
+          transformed_file = transformed_file(source, opts)
+
+          loader = Helper.get_loader(opts['data_loader'])
+
+          loader.load(self, source, transformed_file, opts)
+        end
       end
     end
   end
