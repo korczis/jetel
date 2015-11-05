e40803c25fd0d4b13216b31cab7befc6119bcb3f
Tomas Korcak <korczis@gmail.com>
2015-11-04 23:17:17 -0800

    Added erb and erb_template helpers

--- lib/jetel/helpers/general_helper.rb
+++ lib/jetel/helpers/general_helper.rb
@@ -4,7 +4,9 @@ require_relative '../config/config'
 
 require_relative '../loaders/loaders'
 
+require 'erb'
 require 'i18n'
+require 'ostruct'
 
 module Jetel
   module Helper
@@ -23,6 +25,15 @@ module Jetel
         end
 
         res[:klass].new(uri)
+      end
+
+      def erb(template, vars)
+        ERB.new(template).result(OpenStruct.new(vars).instance_eval { binding })
+      end
+
+      def erb_template(file, vars)
+        template = File.open(file, 'r').read
+        ERB.new(template).result(OpenStruct.new(vars).instance_eval { binding })
       end
     end
   end
