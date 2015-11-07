e5e203f37138f350882aa07b2b4c7891c419ce1f
Tomas Korcak <korczis@gmail.com>
2015-11-07 03:18:27 -0800

    Implemented pure ruby downloader

--- lib/jetel/downloaders/ruby/ruby.rb
+++ lib/jetel/downloaders/ruby/ruby.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
-require 'pty'
+require 'rest-client'
+
+require_relative '../../version.rb'
 
 require_relative '../base_downloader'
 
@@ -10,7 +12,30 @@ module Jetel
       def download(url, opts = BaseDownloader::OPTS_DOWNLOAD)
         super
 
-        puts 'Not implemented'
+        raw = {
+          :headers => {
+            :user_agent => "jetel/#{Jetel::VERSION}"
+          },
+          :method => :get,
+          :url => url,
+          # TODO: Load from config, param or so
+          :verify_ssl => false
+        }
+
+        FileUtils.mkdir_p(opts[:dir])
+
+        filename = opts[:filename] || url.split('/').last
+
+        out_full_path = File.join(opts[:dir], filename)
+
+        File.open(out_full_path, 'w') do |file|
+        RestClient::Request.execute(raw) do |chunk, _x, response|
+            if response.code.to_s != '200'
+              fail ArgumentError, "Error downloading #{url}. Got response: #{response.code} #{response} #{response.body}"
+            end
+            file.write chunk
+          end
+        end
       end
     end
   end
