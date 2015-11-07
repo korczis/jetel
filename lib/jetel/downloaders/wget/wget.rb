132dc09b738bbb27419dd98f44a905cba9974f59
Tomas Korcak <korczis@gmail.com>
2015-11-07 04:00:06 -0800

    Add Wget downloader

---
+++ lib/jetel/downloaders/wget/wget.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+require 'pty'
+
+require_relative '../base_downloader'
+
+module Jetel
+  module Downloaders
+    class Wget < BaseDownloader
+      def download(url, opts = BaseDownloader::OPTS_DOWNLOAD)
+        super
+
+        $stdout.sync = true
+
+        opts = BaseDownloader::OPTS_DOWNLOAD.merge(opts)
+
+        filename = opts[:filename] || url.split('/').last
+
+        cmd = "wget -o \"#{File.join(opts[:dir], filename)}\" #{url}"
+        puts(cmd)
+
+        PTY.spawn(cmd) do |stdout, stdin, pid|
+          begin
+            # Do stuff with the output here. Just printing to show it works
+            stdout.each { |line| print line }
+          end
+        end
+      end
+    end
+  end
+end
