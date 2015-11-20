0635c353144bd6fb266dd5e5d5dae0bd042111bb
Tomas Korcak <korczis@gmail.com>
2015-11-19 22:15:21 -0800

    Add basic helper test

--- spec/helpers/general_helper_spec.rb
+++ spec/helpers/general_helper_spec.rb
@@ -3,3 +3,14 @@
 # Copyright (c) 2010-2015 GoodData Corporation. All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
+require 'jetel/helpers/general_helper'
+
+describe Jetel::Helper do
+  describe '#sanitize' do
+    it 'Sanitizes spaces' do
+      res = Jetel::Helper.sanitize('hello world')
+      expect(res).to eql('hello_world')
+    end
+  end
+end
